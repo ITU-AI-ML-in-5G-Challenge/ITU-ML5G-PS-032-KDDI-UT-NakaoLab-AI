1999617d4d109a97c50d03626354a413786d4465
夏飞 <xiafei571@gmail.com>
2020-08-18 19:45:36 +0900

    first commit

---
+++ json_extract.py
@@ -0,0 +1,58 @@
+import json
+
+path = "./20200629/physical/20200629105300.json"
+all_attributes = []
+
+class Dict(dict):
+    __setattr__ = dict.__setitem__
+    __getattr__ = dict.__getitem__
+
+def dictToObj(dictObj):
+    if not isinstance(dictObj, dict):
+        return dictObj
+    d = Dict()
+    for k, v in dictObj.items():
+        d[k] = dictToObj(v)
+    return d
+
+def return_obj(data):
+    if type(data) is not '__main__.Dict':
+        data = dictToObj(data)
+    return data
+
+def Return_Attribute_List (data):
+    #return list(data.keys())
+    return list(data.keys()), list(data.values())
+
+def Return_All_Atributes(data):
+    data = dictToObj(data)
+    if isinstance(data, list):
+        for item in data:
+            if isinstance(item, dict):
+                attribute_key_list, attribute_value_list = Return_Attribute_List(item)
+                for i in attribute_key_list:
+                    item[i] = dictToObj(item[i])
+                    Return_All_Atributes(item[i])
+            else:
+                if isinstance(data, (int,float)):
+                    all_attributes.append(item)
+    else:
+        if isinstance(data, dict):
+            attribute_key_list, attribute_value_list = Return_Attribute_List (data)
+            for item in attribute_key_list:
+                data[item] = dictToObj(data[item])
+                Return_All_Atributes(data[item])
+        else:
+            if isinstance(data, (int,float)):
+                all_attributes.append(data)
+    #print (all_attributes)
+
+def main():
+    with open(path, 'r') as load_f:
+        data = json.load(load_f)
+        data = dictToObj(data)
+        Return_All_Atributes (data)
+        print(all_attributes)
+
+if __name__ == "__main__":
+    main()
---
+++ json_extract_v2.py
@@ -0,0 +1,67 @@
+import json
+
+path = "./20200629/physical/20200629105300.json"
+all_attributes_value = []
+all_attributes_key = []
+attribute_key = ''
+
+class Dict(dict):
+    __setattr__ = dict.__setitem__
+    __getattr__ = dict.__getitem__
+
+def dictToObj(dictObj):
+    if not isinstance(dictObj, dict):
+        return dictObj
+    d = Dict()
+    for k, v in dictObj.items():
+        d[k] = dictToObj(v)
+    return d
+
+def return_obj(data):
+    if type(data) is not '__main__.Dict':
+        data = dictToObj(data)
+    return data
+
+def Return_Attribute_List (data):
+    #return list(data.keys())
+    return list(data.keys()), list(data.values())
+
+def Return_All_Atributes(data, attribute_key):
+    data = dictToObj(data)
+    if isinstance(data, list):
+        for item in data:
+            #key_ = attribute_key + str(data.index(item))
+            if isinstance(item, dict):
+                attribute_key_list, attribute_value_list = Return_Attribute_List(item)
+                if 'name' in attribute_key_list:
+                    key_ = attribute_key + '#' + attribute_value_list[attribute_key_list.index('name')]
+                else:
+                    key_ = attribute_key + str(data.index(item))
+                for i in attribute_key_list:
+                    item[i] = dictToObj(item[i])
+                    Return_All_Atributes(item[i], key_ + '/' + i)
+            else:
+                if isinstance(data, (int,float)):
+                    all_attributes_value.append(item)
+                    all_attributes_key.append(attribute_key)
+    else:
+        if isinstance(data, dict):
+            attribute_key_list, attribute_value_list = Return_Attribute_List (data)
+            for item in attribute_key_list:
+                data[item] = dictToObj(data[item])
+                Return_All_Atributes(data[item], attribute_key + '/' + item)
+        else:
+            if isinstance(data, (int,float)):
+                all_attributes_value.append(data)
+                all_attributes_key.append(attribute_key)
+
+def main():
+    with open(path, 'r') as load_f:
+        data = json.load(load_f)
+        data = dictToObj(data)
+        Return_All_Atributes (data, attribute_key)
+        print(all_attributes_value)
+        print(all_attributes_key)
+
+if __name__ == "__main__":
+    main()
