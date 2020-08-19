56c526d57f6e9651d11c21e4a497f0588e032804
夏飞 <xiafei571@gmail.com>
2020-08-19 18:29:26 +0900

    增加label code 的获取

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 ./conf/local_conf.yaml 里有json文件的路径，这个配置文件可以随意添加和修改属性
 
 # label type summary
-- ixnetwork-traffic-start: 1
-- node-down: 25
-- node-up: 25
-- interface-down: 110
-- interface-up: 110
-- tap-loss-start: 330
-- tap-loss-stop: 330
-- tap-delay-start: 330
-- tap-delay-stop: 330
-- ixnetwork-bgp-injection-start: 90
-- ixnetwork-bgp-injection-stop: 90
-- ixnetwork-bgp-hijacking-start: 45
-- ixnetwork-bgp-hijacking-stop: 45
-- ixnetwork-traffic-stop: 1
+- 0: ixnetwork-traffic-start: 1
+- 1: node-down: 25
+- 2: node-up: 25
+- 3: interface-down: 110
+- 4: interface-up: 110
+- 5: tap-loss-start: 330
+- 6: tap-loss-stop: 330
+- 7: tap-delay-start: 330
+- 8: tap-delay-stop: 330
+- 9: ixnetwork-bgp-injection-start: 90
+- 10: ixnetwork-bgp-injection-stop: 90
+- 11: ixnetwork-bgp-hijacking-start: 45
+- 12: ixnetwork-bgp-hijacking-stop: 45
+- 13: ixnetwork-traffic-stop: 1
--- label_extract.py
+++ label_extract.py
@@ -1,6 +1,26 @@
 import json
 from datetime import datetime, timedelta
 
+# ixnetwork-traffic-start: 1
+# node-down: 25
+# node-up: 25
+# interface-down: 110
+# interface-up: 110
+# tap-loss-start: 330
+# tap-loss-stop: 330
+# tap-delay-start: 330
+# tap-delay-stop: 330
+# ixnetwork-bgp-injection-start: 90
+# ixnetwork-bgp-injection-stop: 90
+# ixnetwork-bgp-hijacking-start: 45
+# ixnetwork-bgp-hijacking-stop: 45
+# ixnetwork-traffic-stop: 1
+
+
+type_list = ['ixnetwork-traffic-start', 'node-down', 'node-up', 'interface-down', 'interface-up', 'tap-loss-start',
+             'tap-loss-stop', 'tap-delay-start', 'tap-delay-stop', 'ixnetwork-bgp-injection-start',
+             'ixnetwork-bgp-injection-stop', 'ixnetwork-bgp-hijacking-start', 'ixnetwork-bgp-hijacking-stop',
+             'ixnetwork-traffic-stop']
 
 
 class Recipes:
@@ -16,6 +36,9 @@ class Recipes:
             if started_at < date_time < next_started_at:
                 return json_obj["type"]
 
+    def get_type_code(self, file_name):
+        type = self.get_type(file_name)
+        return type_list.index(type)
 
 def load_label(path):
     with open(path, 'r') as label:
@@ -26,6 +49,7 @@ def load_label(path):
 if __name__ == '__main__':
     recipes = load_label("./label-for-learning.json")
     print(recipes.get_type("20200629105300.json"))
+    print(recipes.get_type("20200629105300.json"))
     print(recipes.get_type("20200629105400.json"))
     print(recipes.get_type("20200629105500.json"))
     print(recipes.get_type("20200629105600.json"))
@@ -33,3 +57,14 @@ if __name__ == '__main__':
     print(recipes.get_type("20200629105800.json"))
     print(recipes.get_type("20200629105900.json"))
     print(recipes.get_type("20200629110000.json"))
+
+
+    print(recipes.get_type_code("20200629105300.json"))
+    print(recipes.get_type_code("20200629105300.json"))
+    print(recipes.get_type_code("20200629105400.json"))
+    print(recipes.get_type_code("20200629105500.json"))
+    print(recipes.get_type_code("20200629105600.json"))
+    print(recipes.get_type_code("20200629105700.json"))
+    print(recipes.get_type_code("20200629105800.json"))
+    print(recipes.get_type_code("20200629105900.json"))
+    print(recipes.get_type_code("20200629110000.json"))
