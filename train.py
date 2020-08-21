2639348f8092ddd197d7ab98a206e4384ec4fdf8
夏飞 <xiafei571@gmail.com>
2020-08-21 16:30:53 +0900

    增加行列数量提示

--- train.py
+++ train.py
@@ -57,6 +57,7 @@ pd.set_option('max_colwidth', 100)
 # 读取csv
 dataset = pd.read_csv('./csv/20200629.csv')
 
+print('列数:', dataset.shape[1], '行数:', dataset.shape[0])
 # 划分训练测试
 column = dataset.columns
 X = dataset[column[:-2]]
